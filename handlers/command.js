a63645806bbfd6a25c5b781f1174d35c5860c52f
furatbarzani1 <94864575+furatbarzani1@users.noreply.github.com>
2021-11-23 14:45:25 +0300

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -6,7 +6,7 @@ const ascii = require("ascii-table");
 let table = new ascii("Commands");
 const functions = require("../functions")
 table.setHeading("Command", "Load status");
-console.log("Welcome to HANDLER SERVICE // By jano ")
+console.log("Welcome to HANDLER SERVICE // By CLEVER ")
 module.exports = (client) => {
     readdirSync("./commands/").forEach(dir => {
         const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
@@ -57,30 +57,30 @@ module.exports = (client) => {
                 queue.connection.voice.setDeaf(true);
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
             try {
                 queue.connection.voice.setSelfDeaf(true);
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
             try {
                 functions.playsongyes(client, message, queue, song);
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
         .on("addSong", (message, queue, song) => {
             try {
-                return functions.embedbuilder(client, 7500, message, config.colors.yes, "**Added a Song** <a:jano_29:840650829257637900>", `Song: [\`${song.name}\`](${song.url})  -  \`${song.formattedDuration}\` \n\nRequested by: ${song.user}\n\nEstimated Time: ${queue.songs.length - 1} song(s) - \`${(Math.floor((queue.duration - song.duration) / 60 * 100) / 100).toString().replace(".", ":")}\`\nQueue duration: \`${queue.formattedDuration}\``, song.thumbnail)
+                return functions.embedbuilder(client, 7500, message, config.colors.yes, "**Added a Song** <a:CLEVER:828550389233090571>", `Song: [\`${song.name}\`](${song.url})  -  \`${song.formattedDuration}\` \n\nRequested by: ${song.user}\n\nEstimated Time: ${queue.songs.length - 1} song(s) - \`${(Math.floor((queue.duration - song.duration) / 60 * 100) / 100).toString().replace(".", ":")}\`\nQueue duration: \`${queue.formattedDuration}\``, song.thumbnail)
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972>", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571>", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
@@ -89,32 +89,32 @@ module.exports = (client) => {
                 queue.connection.voice.setDeaf(true);
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
             try {
                 queue.connection.voice.setSelfDeaf(true);
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
-            functions.embedbuilder(client, 7500, message, config.colors.yes, "**Added a Playlist** <a:jano_31:834550131658326055>", `Playlist: [\`${playlist.name}\`](${playlist.url ? playlist.url : ""})  -  \`${playlist.songs.length ? playlist.songs.length : "undefinied"} songs\` \n\nRequested by: ${queue.songs[0].user ? queue.songs[0].user : "error"}`, playlist.thumbnail.url ? playlist.thumbnail.url : "")
+            functions.embedbuilder(client, 7500, message, config.colors.yes, "**Added a Playlist** <a:CLEVER:828550389233090571>", `Playlist: [\`${playlist.name}\`](${playlist.url ? playlist.url : ""})  -  \`${playlist.songs.length ? playlist.songs.length : "undefinied"} songs\` \n\nRequested by: ${queue.songs[0].user ? queue.songs[0].user : "error"}`, playlist.thumbnail.url ? playlist.thumbnail.url : "")
 
             try {
                 functions.playsongyes(client, message, queue, queue.songs[0]);
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, "#ff264a", "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, "#ff264a", "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
         .on("addList", (message, queue, playlist) => {
             try {
-                return functions.embedbuilder(client, 7500, message, config.colors.yes, "**Added a Playlist** <a:jano_31:834550131658326055>", `Playlist: [\`${playlist.name}\`](${playlist.url ? playlist.url : ""})  -  \`${playlist.songs.length ? playlist.songs.length : "undefinied"} songs\` \n\nRequested by: ${queue.songs[0].user ? queue.songs[0].user : "error"}`, playlist.thumbnail.url ? playlist.thumbnail.url : "")
+                return functions.embedbuilder(client, 7500, message, config.colors.yes, "**Added a Playlist** <a:jano_31:>", `Playlist: [\`${playlist.name}\`](${playlist.url ? playlist.url : ""})  -  \`${playlist.songs.length ? playlist.songs.length : "undefinied"} songs\` \n\nRequested by: ${queue.songs[0].user ? queue.songs[0].user : "error"}`, playlist.thumbnail.url ? playlist.thumbnail.url : "")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
@@ -124,7 +124,7 @@ module.exports = (client) => {
                 return functions.embedbuilder(client, "null", message, config.colors.yes, "", `**Choose an option from below**\n${result.map(song => `**${++i}**. [${song.name}](${song.url}) - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`)
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972>", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571>", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
@@ -134,14 +134,14 @@ module.exports = (client) => {
                 message.react("<a:jano_26:799630865474256972>")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972>", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571>", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
             try {
                 return functions.embedbuilder(client, 5000, message, config.colors.yes, `Searching canceled`, "")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
@@ -151,31 +151,31 @@ module.exports = (client) => {
                 message.react("<a:jano_26:799630865474256972>")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
             console.log(err);
             try {
-                return functions.embedbuilder(client, "null", message, config.colors.yes, "**An error encountered:** <a:jano_26:799630865474256972>", "```" + err + "```")
+                return functions.embedbuilder(client, "null", message, config.colors.yes, "**An error encountered:** <a:CLEVER:828550389233090571>", "```" + err + "```")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
         .on("finish", message => {
             try {
-                return functions.embedbuilder(client, 5000, message, config.colors.yes, "**Left Channel** <a:jano_26:799630865474256972>", "**There are no more songs left**")
+                return functions.embedbuilder(client, 5000, message, config.colors.yes, "**Left Channel** <a:CLEVER:828550389233090571>", "**There are no more songs left**")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR**<a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })
         .on("empty", message => {
 
             try {
-                return functions.embedbuilder(client, 5000, message, config.colors.yes, "**Left the channel cause it got empty**<a:jano_26:799630865474256972>")
+                return functions.embedbuilder(client, 5000, message, config.colors.yes, "**Left the channel cause it got empty**<a:CLEVER:828550389233090571>")
             } catch (error) {
                 console.error(error)
                 functions.embedbuilder(client, 5000, message, config.colors.no, "ERROR: ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
@@ -187,7 +187,7 @@ module.exports = (client) => {
                 return functions.embedbuilder(client, 5000, message, config.colors.yes, "**Can't find related video to play. Stop playing music**<a:jano_26:799630865474256972>")
             } catch (error) {
                 console.error(error)
-                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:jano_26:799630865474256972> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
+                functions.embedbuilder(client, 5000, message, config.colors.no, "**ERROR** <a:CLEVER:828550389233090571> ", "```" + error.toString().substr(0, 100) + "```" + "\n\n**Error got sent to my owner!**")
                 functions.errorbuilder(error.stack.toString().substr(0, 2000))
             }
         })

1fc9135b7c1e857da6ed370b54f7f101e598f8a8
furatbarzani1 <94864575+furatbarzani1@users.noreply.github.com>
2021-11-23 14:24:45 +0300

    Update prefix.js

--- commands/INFORMATION COMMANDS/prefix.js
+++ commands/INFORMATION COMMANDS/prefix.js
@@ -10,15 +10,15 @@ module.exports = {
     //command
     let prefix = client.settings.get(message.guild.id, `prefix`);
     if (prefix === null) prefix = config.prefix;
-    message.react("<a:jano_19:840480433754079242>");
+    message.react("<a:CLEVER:828550389233090571>");
     if (!args[0]) return functions.embedbuilder(client, "null", message, "YELLOW", `Current prefix: \`${prefix}\``, `Please provide a new prefix`)
-    if (!message.member.hasPermission("ADMINISTRATOR")) return functions.embedbuilder(client, "null", message, config.colors.no, "prefix", `<a:jano_26:799630865474256972> You don\'t have permission for this Command!`)
+    if (!message.member.hasPermission("ADMINISTRATOR")) return functions.embedbuilder(client, "null", message, config.colors.no, "prefix", `<a:CLEVER:828550389233090571> You don\'t have permission for this Command!`)
 
-    if (args[1]) return functions.embedbuilder(client, "null", message, config.colors.no, "prefix", `<a:jano_26:799630865474256972> The prefix can\'t have two spaces`)
-    if (args[0].length > 5) return functions.embedbuilder(client, "null", message, config.colors.no, "ERROR", `<a:jano_26:799630865474256972> The prefix can\'t be Longer then "5"`)
+    if (args[1]) return functions.embedbuilder(client, "null", message, config.colors.no, "prefix", `<a:CLEVER:828550389233090571> The prefix can\'t have two spaces`)
+    if (args[0].length > 5) return functions.embedbuilder(client, "null", message, config.colors.no, "ERROR", `<a:CLEVER:828550389233090571> The prefix can\'t be Longer then "5"`)
 
     client.settings.set(message.guild.id, args[0], `prefix`);
 
-    return functions.embedbuilder(client, "null", message, config.colors.yes, "prefix", `<a:jano_19:840480433754079242> Successfully set new prefix to **\`${args[0]}\`**`)
+    return functions.embedbuilder(client, "null", message, config.colors.yes, "prefix", `<a:CLEVE:828550389233090571> Successfully set new prefix to **\`${args[0]}\`**`)
   }
 };

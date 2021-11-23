89d068a24ae58e161490a53af47ade2ab8da0467
furatbarzani1 <94864575+furatbarzani1@users.noreply.github.com>
2021-11-23 14:07:48 +0300

    Update github.js

--- commands/INFORMATION COMMANDS/github.js
+++ commands/INFORMATION COMMANDS/github.js
@@ -11,17 +11,17 @@ module.exports = {
 	description: "Shows you the Github and Source Code Information about this Bot",
 	run: async (client, message, args, user, text, prefix) => {
 			message.channel.send(new MessageEmbed()
-				.setTitle(`This Bot is made by \`JANO#3735\` and **this** is the Source Code link to this Bot`)
-				.setURL("https://github.com/Devjano")
+				.setTitle(`This Bot is made by \`<@828550389233090571>\` and **this** is the Source Code link to this Bot`)
+				.setURL("")
 				.addField("Discord.js: ", "[\`v12.5.1\`](https://discord.js.org)", true)
 				.addField("FFMPEG: ", "\`v4.1.6-1\`", true)
 				.addField("Node.js: ", "[\`v15.3.4\`](https://nodejs.org/en/)", true)
 				.addField("Distube: ", "[\`v2.8.7\`](https://distube.js.org)")
-				.addField("Source Code. ", "Don't just use the source for yourself,... please [ invite ](https://discord.com/api/oauth2/authorize?client_id=778276774320275487&permissions=0&scope=bot) me too![\`Click here\`](https://discord.com/api/oauth2/authorize?client_id=778276774320275487&permissions=0&scope=bot)")
-			        .addField("Source Code. ", "[ My git hub ](https://github.com/Devjano) link git hub ![\`Click here\`](https://github.com/Devjano)")
+				.addField("Source Code. ", "Don't just use the source for yourself,... please [ invite ](https://discord.com/oauth2/authorize?client_id=911935319375499284&permissions=70282305&scope=bot) me too![\`Click here\`](https://discord.com/oauth2/authorize?client_id=911935319375499284&permissions=70282305&scope=bot)")
+			        .addField("Source Code. ", "[ My git hub ]() link git hub ![\`Click here\`]()")
 				
 				.setColor(config.colors.yes)
-				.setFooter("Hyper Canary | by: jano", client.user.displayAvatarURL())
+				.setFooter("Hyper Canary | by: CLEVER", client.user.displayAvatarURL())
 			);
 	}
 }
